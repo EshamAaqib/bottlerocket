5b752e9d834b857c3424c540f3e7f3585b49de75
Esham Aaqib <esham.aaqib@h2o.ai>
2024-02-29 16:30:13 +0530

    revert

--- packages/build.rs
+++ packages/build.rs
@@ -1,7 +1,6 @@
 use std::process::{exit, Command};
 
 fn main() -> Result<(), std::io::Error> {
-    std::env::set_var("build-id", "01");
     let ret = Command::new("buildsys").arg("build-package").status()?;
     if !ret.success() {
         exit(1);
