d23b9a97e4a9e565a7d933367c6982f2c58bdd9d
Esham Aaqib <esham.aaqib@h2o.ai>
2024-02-29 16:27:09 +0530

    test

--- packages/build.rs
+++ packages/build.rs
@@ -1,6 +1,7 @@
 use std::process::{exit, Command};
 
 fn main() -> Result<(), std::io::Error> {
+    std::env::set_var("BUILD_ID", "01");
     let ret = Command::new("buildsys").arg("build-package").status()?;
     if !ret.success() {
         exit(1);
